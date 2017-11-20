3f1d1cd421d431c7eb9ae5b164af3c632d5c458a
André Hanak <impressum@a-hanak.de>
2017-11-20 21:13:28 +0100

    Added Optimizations

--- cmake/stm32l4.cmake
+++ cmake/stm32l4.cmake
@@ -19,10 +19,11 @@ set(CMAKE_ASM_COMPILER arm-none-eabi-g++)
 set(CMAKE_OBJCOPY arm-none-eabi-objcopy)
 
 #set(COMMON_FLAGS "-mcpu=${MCU_ARCH} -mthumb -mthumb-interwork -mfloat-abi=${MCU_FLOAT_ABI} -ffunction-sections -fdata-sections -g -fno-common -fmessage-length=0")
-set(COMMON_FLAGS "-mcpu=${MCU_ARCH} -mthumb -mfloat-abi=${MCU_FLOAT_ABI} -ffunction-sections -fdata-sections -fno-common")
+set(COMMON_FLAGS "-O3 -mcpu=${MCU_ARCH} -mthumb -mfloat-abi=${MCU_FLOAT_ABI} -ffunction-sections -fdata-sections -fno-common")
 
 if (MCU_FLOAT_ABI STREQUAL hard)
     set(COMMON_FLAGS "${COMMON_FLAGS} -mfpu=${MCU_FPU}")
 endif ()
 
-set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} ${COMMON_FLAGS}")
+set(CMAKE_CXX_FLAGS "${COMMON_FLAGS} ${CMAKE_CXX_FLAGS}")
+set(CMAKE_C_FLAGS "${COMMON_FLAGS} ${CMAKE_C_FLAGS}")
